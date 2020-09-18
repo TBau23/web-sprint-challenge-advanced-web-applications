59e4f50e480fe68345d50e900055e3dc0841d793
Tom Bauer <tbauer1898@gmail.com>
2020-09-18 13:59:56 -0500

    Tweaked test.

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -12,13 +12,36 @@ const mockData = [
       hex: '#f0f8ff'
     },
     id: 1
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc"
+    },
+    id: 2
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff"
+    },
+    id: 3
+  },
+  {
+    color: "aquamarine",
+    code: {
+      hex: "#7fffd4"
+    },
+    id: 4
   }
 ] 
 
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
   
+
   fetchColors.mockResolvedValueOnce(mockData)
   render(<BubblePage />)
+
 
 });
